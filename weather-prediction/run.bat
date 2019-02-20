0acf76a69444d42fc632e8a1ba07580ffcd1a4db
emilycooke <emily.cooke@bluwave-ai.com>
2019-02-20 14:08:11 -0500

    Add scikit-learn package to energy-prediction requirements. No longer support for Windows.

--- weather-prediction/run.bat
+++
@@ -1,6 +0,0 @@
-python src/data/make_dataset.py
-python src/features/build_features.py
-python src/features/select_features.py
-python src/models/train_model.py
-python src/models/predict_model.py
-python src/visualization/visualize.py
